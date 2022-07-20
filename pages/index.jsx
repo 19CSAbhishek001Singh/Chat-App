7246aab85e6cc2d97c88a1601f4de7334739278e
Abhishek <abhisheksinghcsanand@gmail.com>
2022-07-20 18:58:19 +0530

    done

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image"
 const Home = () => {
   return (
-    <div className="flex mt-0 mb-0 bottom-0 h-screen w-screen place-content-center items-center bg-black">
+    <div className="flex mt-0 mb-0 bottom-0 h-screen w-screen place-content-center items-center lg:bg-black md:bg-black bg-gray-900">
       <Head>
         <title>Real Time Chat App</title>
         <link rel="icon" href="/image_processing20210104-18320-1qp0a4c.png" />
